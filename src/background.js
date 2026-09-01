7b75ce4c6705e134da5a8c661e11df70c64bce35
Ds.Megumiism <58034346+DSmegumi@users.noreply.github.com>
2026-09-01 15:35:10 +0800

    feat: load response limit configuration layer

--- src/background.js
+++ src/background.js
@@ -1,1 +1,1 @@
-importScripts("lib/db.js", "lib/targets.js", "background/00-core.js", "background/10-capture.js", "background/20-events.js");
+importScripts("lib/db.js", "lib/targets.js", "background/00-core.js", "background/05-response-limits.js", "background/10-capture.js", "background/20-events.js");
